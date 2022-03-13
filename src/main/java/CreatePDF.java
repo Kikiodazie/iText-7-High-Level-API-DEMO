af3c91c5f91a401264207755f37e2775219b0f24
Kikiodazie <kiki.odazie@yahoo.com>
2022-03-13 23:07:47 +0100

    Create a size A4 PDF Document

--- src/main/java/CreatePDF.java
+++ src/main/java/CreatePDF.java
@@ -1,2 +1,27 @@
+import com.itextpdf.kernel.geom.PageSize;
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.layout.Document;
+
+import java.io.FileNotFoundException;
+
 public class CreatePDF {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        String pdfName = "invoice.pdf";
+        PdfWriter pdfWriter = new PdfWriter(pdfName);
+        PdfDocument pdfDocument = new PdfDocument(pdfWriter);
+        pdfDocument.addNewPage();
+        Document document = new Document(pdfDocument);
+
+        // First set size of page.
+        pdfDocument.setDefaultPageSize(PageSize.A4);
+
+
+
+        document.close();
+        System.out.println("Your PDF invoice has been created!!!");
+    }
+
 }
+
